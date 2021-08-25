a0d516060163e053e3632087b664a7f5a2470b07
Kartik Kshirsagar <kartikpkshirsagar@gmail.com>
2021-08-25 14:49:11 +0530

    implemented core logic

---
+++ main.py
@@ -0,0 +1,81 @@
+from models import Board
+
+
+
+def checkVisited(node, visited):
+    if node in visited:
+        return True
+    return False
+
+
+def constructPath(node, src, goal, visited, type):
+    path = []
+    reverseNode = None
+    for e in visited:
+        if node.config == e.config:
+            reverseNode = e
+    if reverseNode is not None:
+        path.append(node)
+        if type == 1:
+            temp = node
+        elif type == 2:
+            temp = reverseNode
+        while temp.config != src.config:
+            temp = temp.parent
+            path.insert(0, temp)
+        if type == 1:
+            temp = reverseNode
+        elif type == 2:
+            temp = node
+        while temp.config != goal.config:
+            temp = temp.parent
+            path.append(temp)
+    return path
+
+
+def biDirectionalBFS(src, goal):
+    visited = set()
+    reverseVisited = set()
+    queue = []
+    reverseQ = []
+    queue.append(src)
+    reverseQ.append(goal)
+
+    while queue and reverseQ:
+        # forward step first
+        poppedF = queue.pop(0)
+        if checkVisited(poppedF, reverseVisited):
+            # we got the connected node
+            return constructPath(poppedF, src, goal, reverseVisited, 1)
+        visited.add(poppedF)
+        neighbours = poppedF.getNeighbours()
+        for neighbour in neighbours:
+            if not checkVisited(neighbour, visited):
+                queue.append(neighbour)
+                # this to avoid reading the nodes which are already in the queue
+                visited.add(neighbour)
+        # backward step
+        poppedR = reverseQ.pop(0)
+        if checkVisited(poppedR, visited):
+            # got goal
+            return constructPath(poppedR, src, goal, visited, 2)
+
+        reverseVisited.add(poppedR)
+        neighboursR = poppedR.getNeighbours()
+        for neighbour in neighboursR:
+            if not checkVisited(neighbour, reverseVisited):
+                reverseQ.append(neighbour)
+                reverseVisited.add(neighbour)
+    print("This instance of the puzzle is unsolvable.")
+    return []
+
+def main():
+    srcBoard = Board((("4", "6", "1"), ("_", "2", "8"), ("7", "3", "5")))
+    goalBoard = Board((("1", "2", "3"), ("4", "_", "6"), ("7", "8", "5")))
+    path = biDirectionalBFS(srcBoard, goalBoard)
+    for x in path:
+        x.print()
+
+
+if __name__ == "__main__":
+    main()
---
+++ models.py
@@ -0,0 +1,55 @@
+class Board:
+    def __init__(self, _config):
+        self.config = _config  # config should be a 3*3 matrix
+        self.parent = None
+
+    def print(self):
+        print('\n'.join(['\t'.join([str(cell) for cell in row]) for row in self.config]))
+        print("\n")
+
+    def __eq__(self,other):
+        if isinstance(other,Board):
+            return self.config == other.config
+        return False
+    def __hash__(self):
+        return hash(self.config)
+    def getNeighbours(self):
+        ret_list = []
+        gap = (0, 0)
+        for i in range(3):
+            for j in range(3):
+                if self.config[i][j] == "_":
+                    gap = (i, j)
+        if gap[0] > 0:
+            newConf = [list(row[:]) for row in self.config]
+            newConf[gap[0]][gap[1]] = newConf[gap[0]-1][gap[1]]
+            newConf[gap[0]-1][gap[1]] = "_"
+            newConft = tuple(tuple(row[:]) for row in newConf)
+            board = Board(newConft)
+            board.parent = self
+            ret_list.append(board)
+        if gap[0] < 2:
+            newConf = [list(row[:]) for row in self.config]
+            newConf[gap[0]][gap[1]] = newConf[gap[0]+1][gap[1]]
+            newConf[gap[0]+1][gap[1]] = "_"
+            newConft = tuple(tuple(row[:]) for row in newConf)
+            board = Board(newConft)
+            board.parent = self
+            ret_list.append(board)
+        if gap[1] > 0:
+            newConf = [list(row[:]) for row in self.config]
+            newConf[gap[0]][gap[1]] = newConf[gap[0]][gap[1]-1]
+            newConf[gap[0]][gap[1]-1] = "_"
+            newConft = tuple(tuple(row[:]) for row in newConf)
+            board = Board(newConft)
+            board.parent = self
+            ret_list.append(board)
+        if gap[1] < 2:
+            newConf = [list(row[:]) for row in self.config]
+            newConf[gap[0]][gap[1]] = newConf[gap[0]][gap[1]+1]
+            newConf[gap[0]][gap[1]+1] = "_"
+            newConft = tuple(tuple(row[:]) for row in newConf)
+            board = Board(newConft)
+            board.parent = self
+            ret_list.append(board)
+        return ret_list
